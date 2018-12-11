4f337ed280b2f0b2898b746d0687a667d010d357
Michael Porter <miporterg@gmail.com>
2018-12-11 20:03:39 +0800

    .

--- accounts/forms.py
+++ accounts/forms.py
@@ -64,3 +64,65 @@ class MyPasswordChangeForm(PasswordChangeForm):
                 code='password_incorrect',
             )
         return old_password
+
+
+
+#登录表单
+class UserForm(forms.Form):
+    email = forms.EmailField(required=True,max_length=100,widget=forms.TextInput(attrs={'type':'text','placeholder':'请输入您的用户名'}))
+    password = forms.CharField(min_length=6,widget=forms.TextInput(attrs={'type':'password','placeholder':'请输入您的密码'}))
+    def clean_username(self):
+        email = self.cleaned_data.get('email')
+        if email_check(email):
+            filter_result = User.objects.filter(email=email)
+            if not filter_result:
+                raise forms.ValidationError("email not found.")
+        return email
+
+
+class RegistrationForm(forms.Form):
+    username = forms.CharField(required=True,max_length=10)
+    email = forms.EmailField(required=True,max_length=100)
+    password = forms.CharField(min_length=6)
+    confirm = forms.CharField(min_length=6)
+
+    def clean_username(self):
+        username=self.cleaned_data.get('username')
+       
+        if username_check(username):
+            filter_result = User.objects.filter(username=username)
+            if len(filter_result) > 0:
+                raise forms.ValidationError("username already taken.")
+            return username
+        else:
+            raise forms.ValidationError("username has illegal characters.")
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if email_check(email):
+            filter_result = User.objects.filter(email=email)
+            if len(filter_result) > 0:
+                raise forms.ValidationError("email already taken.")
+        else:
+            raise forms.ValidationError("enter a valid email address.")
+
+        return email
+
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+
+        if len(password) < 6:
+            raise forms.ValidationError("password too short.")
+        elif len(password) > 20:
+            raise forms.ValidationError("Your password is too long.")
+
+        return password
+
+    def clean_confirm(self):
+        password = self.cleaned_data.get('password')
+        confirm = self.cleaned_data.get('confirm')
+
+        if password and confirm and password != confirm:
+            raise forms.ValidationError("Password mismatch.")
+        return confirm
